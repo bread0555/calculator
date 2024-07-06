c78931010f246d1246b49c11daf5d65b7c5b5ef2
breadbrawlstars <8762263-breadbrawlstars@users.noreply.replit.com>
2024-07-06 11:05:56 +0000

    start div method, remove trailing 0s from output in subt method

--- number.py
+++ number.py
@@ -7,14 +7,17 @@ class Number:
     def add(self, a, b):
         a = str(a)
         b = str(b)
+
         if len(a) < len(b):
             a = "0" * (len(b) - len(a)) + a
         elif len(b) < len(a):
             b = "0" * (len(a) - len(b)) + b
+
         a = a[::-1]
         b = b[::-1]
         spare = 0
         output = ""
+
         for i in range(len(a)):
             result = int(a[i]) + int(b[i]) + spare
             if result == 0:
@@ -29,42 +32,67 @@ class Number:
             elif result == 3:
                 output += "1"
                 spare = 1
+
         if spare == 1:
             output += "1"
+
         return output[::-1]
 
     def subt(self, a, b):
         a = str(a)
         b = str(b)
+
         if len(a) < len(b):
             a = "0" * (len(b) - len(a)) + a
         elif len(b) < len(a):
             b = "0" * (len(a) - len(b)) + b
+
         b = b[::-1]
         b_new = ""
+
         for i in range(len(b)):
             if b[i] == "0":
                 b_new += "1"
             elif b[i] == "1":
                 b_new += "0"
-        b_new = b_new[::-1]
-        b_new = self.add(b_new, "1")
-        return self.add(a, b_new)[-len(b):]
+
+        b_new = self.add(b_new[::-1], "1")
+        output = self.add(a, b_new)[-len(b):]
+
+        one_location = 0
+
+        for i in range(len(output)):
+            if output[i] == "1":
+                one_location = i
+                break
+
+        return output[one_location:]
 
     def mult(self, a, b):
         a = str(a)
         b = str(b)[::-1]
         output = 0
+
         for i in range(len(b)):
             if b[i] == "1":
                 output = self.add(output, a + "0" * i)
 
-        return output
+        return str(output)
 
     def div(self, a, b):
-        pass
+        a = str(a)
+        b = str(b)
 
-    def operation(self, a, b):
+        if b == 0:
+            return 0
+        elif len(a) < len(b):
+            return "0"
+        elif a == b:
+            return "1"
+        else:
+            pass
+
+    def operate(self, a, b):
         if self.operator == "+":
             self.add(a, b)
         elif self.operator == "-":
@@ -73,3 +101,6 @@ class Number:
             self.mult(a, b)
         elif self.operator == "/":
             self.div(a, b)
+
+
+number = Number(1101, 1011, "+")
